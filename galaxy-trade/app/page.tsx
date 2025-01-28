8f2786b142753f5b62b403d813f96ab0a752004d
Joseph Stone <stonepreston05@gmail.com>
2025-01-27 22:03:18 -0700

    started on the inventory component starting testing on query to get the items from the user

--- galaxy-trade/app/page.tsx
+++ galaxy-trade/app/page.tsx
@@ -3,11 +3,13 @@
 'use client';
 
 import { FC } from 'react';
+import Inventory from './components/InventoryPanel';
 
 const HomePage: FC = () => {
   return (
     <div>
       <h1>Welcome to GalaxyTrade 🌌</h1>
+      <Inventory/>
       <p>This is the home page where you can list items available for trade.</p>
     </div>
   );
