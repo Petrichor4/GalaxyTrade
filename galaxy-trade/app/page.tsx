d0b78f7b31e7d9c32221afb9b1e4dc9b13def51d
Joseph Stone <stonepreston05@gmail.com>
2025-01-28 20:27:10 -0700

    finished the inventory component and added login check

--- galaxy-trade/app/page.tsx
+++ galaxy-trade/app/page.tsx
@@ -2,17 +2,14 @@
 
 'use client';
 
-import { FC } from 'react';
 import Inventory from './components/InventoryPanel';
 
-const HomePage: FC = () => {
+export default function HomePage() {
   return (
     <div>
       <h1>Welcome to GalaxyTrade 🌌</h1>
-      <Inventory/>
+      <Inventory username={"testUser"} />
       <p>This is the home page where you can list items available for trade.</p>
     </div>
   );
 };
-
-export default HomePage;
